9d15e7ee932e49f54624112e90377e756875f788
Adam Cooper <adam@acpr.dev>
2020-01-20 10:48:52 +0000

    Backport into php71-compat with newest test changes from master

--- src/Context/RuntimeMinkContext.php
+++ src/Context/RuntimeMinkContext.php
@@ -4,16 +4,18 @@ declare(strict_types=1);
 
 namespace Acpr\Behat\Psr\Context;
 
-use Behat\Mink\Session;
+use Behat\Mink\Session as MinkSession;
+use Behat\MinkExtension\Context\RawMinkContext;
+use RuntimeException;
 
 trait RuntimeMinkContext
 {
     /**
-     * @var Session
+     * @var MinkSession
      */
     private $minkSession;
 
-    public function setMinkSession(Session $session): void
+    public function setMinkSession(MinkSession $session): void
     {
         $this->minkSession = $session;
     }
@@ -23,6 +25,20 @@ trait RuntimeMinkContext
      */
     public function runtimeMinkSession()
     {
+        if (!($this instanceof RawMinkContext)) {
+            throw new RuntimeException(
+                'The \Acpr\Behat\Psr\Context\RuntimeMinkContext trait can only be used by a context that ' .
+                 'extends \Behat\MinkExtension\Context\RawMinkContext'
+            );
+        }
+
+        if ($this->minkSession === null) {
+            throw new RuntimeException(
+                'The context has not been initialized correctly, please ensure it implements ' .
+                '\Acpr\Behat\Psr\Context\Psr11MinkAwareContext'
+            );
+        }
+
         $this->getMink()->registerSession('psr', $this->minkSession);
         $this->getMink()->resetSessions();
     }
